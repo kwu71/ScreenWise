f359b046d99a541108a1cee4b1fc7037a53875ad
JorYin <134739719+JorYin@users.noreply.github.com>
2024-02-07 16:54:41 -0500

    finish Dash

--- src/client/ScreenWiseProject/src/pages/dashboard/dashboard.jsx
+++ src/client/ScreenWiseProject/src/pages/dashboard/dashboard.jsx
@@ -11,16 +11,22 @@ function Dashboard() {
     <div>
       <div><Navbar /></div>  
 
-    <div class={styles.chartContainer}>
-      <img class={styles.chartImg} src="https://images.unsplash.com/photo-1591696205602-2f950c417cb9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Z3JhcGh8ZW58MHx8MHx8fDA%3D" />
-    </div>
+      <div class={styles.chartContainer}>
+        <img class={styles.chartImg} src="https://images.unsplash.com/photo-1591696205602-2f950c417cb9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Z3JhcGh8ZW58MHx8MHx8fDA%3D" />
+      </div>
 
-    <div>
-      <form class={styles.formContainer}>
-        <input class={styles.inputContainer} type="number"></input>
-        <button class={styles.formBTN} >Submit</button>
-      </form>
-    </div>
+      <div class={styles.inputSection}>
+        
+        <h1 class={styles.inputTitle} >Input amount of hours on social media today</h1>
+
+        <div class={styles.formContainer}>
+          <form >
+            <input class={styles.inputContainer} type="number" />
+            <button class={styles.formBTN} >Submit</button>
+          </form>
+        </div>
+      
+      </div>
     
     
     
