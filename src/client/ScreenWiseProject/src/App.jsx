cf20bcfbf1264f71a482848a4cffc4e0764680b2
JorYin <134739719+JorYin@users.noreply.github.com>
2024-02-10 17:09:16 -0500

    .

--- src/client/ScreenWiseProject/src/App.jsx
+++ src/client/ScreenWiseProject/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landingPage/landingPage'
 import Login from './pages/login/login'
 import Dashboard from './pages/dashboard/dashboard'
-import Leaderboard from './pages/leaderboard/leaderboard'
+import Tips from './pages/tips/tipsPage';
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element= {<LandingPage />}/>
           <Route path="/login" element= {<Login />}/>
           <Route path="/dashboard" element= {<Dashboard />}/>
-          <Route path="/leaderboard" element= {<Leaderboard />}/>
+          <Route path="/tips" element= {<Tips />}/>
         </Routes>
       </BrowserRouter>
     </>
