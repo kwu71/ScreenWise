5987ec85c8973ade6705bab274e520fd09acdc0b
JorYin <134739719+JorYin@users.noreply.github.com>
2024-03-06 22:37:05 -0500

    Friend Add

--- src/client/ScreenWiseProject/src/pages/friends/friendsAdd.jsx
+++ src/client/ScreenWiseProject/src/pages/friends/friendsAdd.jsx
@@ -1,6 +1,6 @@
 import FriendNavBar from '../../component/friendNav/friendNavBar';
 import Navbar from '../../component/navBar/navBar';
-import styles from './friends.module.css'
+import styles from './friendsAdd.module.css';
 
 function FriendsAdd() {
   return(
@@ -12,6 +12,24 @@ function FriendsAdd() {
         
         <div className={styles.friendNavContainer}>
           <FriendNavBar />
+        </div>
+        
+        <div class={styles.friendsBlockRight}>
+        
+            <div class={styles.headingBackground}>
+              <div class={styles.addFriendContainer}>
+                <h1>Add a Friend!</h1>
+                <p class={styles.addFriendContainerPTag}>You can add people on ScreenWise with their username!</p>
+              </div>
+            </div>
+
+            <div class={styles.formContainerFriend}>
+              <form >
+                <input class={styles.inputContainerFriend} type="text" placeholder='@username'/>
+                <button class={styles.friendBTN} >Submit</button>
+              </form>
+            </div>
+        
         </div>
 
       </div>
