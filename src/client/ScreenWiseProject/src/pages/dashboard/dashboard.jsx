dbc59f4e0ea4ddd2211b44756291808248296c46
JorYin <134739719+JorYin@users.noreply.github.com>
2024-07-19 12:56:25 -0500

    Dashboard done

--- src/client/ScreenWiseProject/src/pages/dashboard/dashboard.jsx
+++ src/client/ScreenWiseProject/src/pages/dashboard/dashboard.jsx
@@ -32,11 +32,20 @@ function Dashboard() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+
+      if (numberOfHours < 0) {
+        alert('Number of hours cannot be less than 0');
+        setNumberOfHours(0);
+        return;
+      }
+      
       setPending(true);
       const dataToSend = {hours : numberOfHours}
       const response = await Axios.post(`http://localhost:3000/api/users/addHours/${user._id}`, dataToSend);
       setNumberOfHours(0);
+      
       setPending(false);
+      
       getScreenTime();
     } catch (error) {
       setPending(false);
@@ -56,19 +65,46 @@ function Dashboard() {
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-[75vh]'>
         <div className='w-full max-w-xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md'>
           <div>
-            {!isPendingTime && <h1>Hours: {totalScreenTime}</h1>}
+            {!isPendingTime && 
+              <>
+                <div className='flex flex-col text-center'>
+                  <h1 className='text-xl'>Hours</h1> 
+                  <p className='text-7xl'>{totalScreenTime}</p>
+                </div>
+              </>
+            }
             {isPendingTime && <h1>Fetching Time {totalScreenTime}</h1>}
           </div>
 
-          <div>
-            <h1>Input amount of hours on social media today</h1>
+          <div className='mt-10'>
+            <h1 className='font-bold'>Input amount of hours on social media today</h1>
           </div>
 
           <div>
-            <form onSubmit={handleSubmit}>
-              <input type="number" value={numberOfHours} onChange={handleNumberChange}/>
-              {!isPending && <button type="submit">Submit</button>}
-              {isPending && <button type="submit" disabled>Sending Hours...</button>}
+            <form onSubmit={handleSubmit} className='flex flex-col'>
+              <input type="text" inputmode="numeric" value={numberOfHours} onChange={handleNumberChange} className='p-1 border-2'/>
+              {!isPending && 
+                <>
+                <div className='flex items-center justify-center mt-10'>
+                  <button type="submit" className='px-10 py-2 rounded-lg bg-gray-300 transition ease-in-out delay-[50ms] hover:bg-gray-400'>
+                    Submit
+                  </button>
+                </div>                
+                </>
+                }
+              {isPending && 
+                <>
+                  <div className='flex items-center justify-center mt-10'>
+                    <button type="submit" className='inline-flex items-center px-10 py-2 rounded-lg bg-gray-300 transition ease-in-out delay-[50ms] hover:bg-gray-400'>
+                      <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
+                      Submit
+                    </button>
+                  </div>                
+                </>
+                }
             </form>
           </div>
         </div>      
