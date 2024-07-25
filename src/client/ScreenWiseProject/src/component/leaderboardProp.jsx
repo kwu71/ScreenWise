db6bfc48232565e85dc4c599e42fdc6309930d9a
JorYin <134739719+JorYin@users.noreply.github.com>
2024-07-25 14:43:34 -0500

    Minor styling for leaderboard comp

--- src/client/ScreenWiseProject/src/component/leaderboardProp.jsx
+++ src/client/ScreenWiseProject/src/component/leaderboardProp.jsx
@@ -5,12 +5,12 @@ function LeaderboardProp (props) {
     <div className="mt-2 w-full max-w-6xl mx-auto">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          <p className="px-4 py-1 bg-slate-200 rounded-lg">{props.rank}</p>
-          <p className="ml-10">@{props.username}</p>
+          <p className="px-4 py-1 bg-slate-200 rounded-lg font-extrabold">{props.rank}</p>
+          <p className="ml-10 font-extrabold">@{props.username}</p>
         </div>
         <div>
-          {props.hours <= 9 && <p className="mr-5">{props.hours}</p>}
-          {props.hours >= 10 && <p className="mr-4">{props.hours}</p>}
+          {props.hours <= 9 && <p className="mr-5 font-extrabold">{props.hours}</p>}
+          {props.hours >= 10 && <p className="mr-4 font-extrabold">{props.hours}</p>}
         </div>
       </div>
     </div>
