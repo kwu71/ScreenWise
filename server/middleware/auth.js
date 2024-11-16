00147250f0614514b1b927190c943521d106c03b
JorYin <134739719+JorYin@users.noreply.github.com>
2024-11-16 11:12:38 -0500

    .

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,10 +4,6 @@ const isAuthenticated = (req,res, next) => {
   if(req.isAuthenticated()){
     return next();
   } else {
-    res.set({
-      'Access-Control-Allow-Origin': 'https://screen-wise.vercel.app',
-      'Access-Control-Allow-Credentials': 'true'
-    });
     res.redirect('https://screen-wise.vercel.app/login');
   }
 }
